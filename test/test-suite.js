da5c8c83c18fbf12005879e6095c80ba7b904159
Jeremy Menetrier <jeremy.menetrier@gmail.com>
2014-11-20 17:46:54 +0100

    Transformed the module to a class and added new tests

--- test/test-suite.js
+++ test/test-suite.js
@@ -1,7 +1,40 @@
-var expect = require('chai').expect;
+'user strict'
 
-describe('test', function() {
-  it('pass', function() {
+var expect = require('chai').expect;
+var mockFs = require('mock-fs');
+var safemod = require('../lib/safemod')
+
+before(function() {
+  // Set up File-system mock
+
+  mockFs({
+    'root': {
+      'first-file': 'first content',
+
+      'foo': {
+        'bar': {
+          'second-file': 'second content'
+        },
+
+        'baz': {
+          'end': {}
+        }
+      },
+    }
+  });
+});
+
+after(function() {
+  // Restore File-system mock
+  mockFs.restore();
+});
+
+describe('Safemod', function() {
+  it('transform a directory list to a an object', function() {
+    expect(1).to.be.ok;
+  });
+
+  it('deep merge an object properties to an other', function() {
     expect(1).to.be.ok;
   });
 });
